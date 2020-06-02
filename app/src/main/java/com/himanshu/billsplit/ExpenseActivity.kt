ab4e1fb60ce0aae9d58496fffff2c770d0e30297
himanshu-yadav-1608 <ydv1608@gmail.com>
2020-06-03 04:08:59 +0530

    Changed Bundle

--- app/src/main/java/com/himanshu/billsplit/ExpenseActivity.kt
+++ app/src/main/java/com/himanshu/billsplit/ExpenseActivity.kt
@@ -3,6 +3,7 @@ package com.himanshu.billsplit
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.forEachIndexed
 import androidx.databinding.DataBindingUtil
@@ -20,28 +21,49 @@ class ExpenseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@ExpenseActivity,R.layout.activity_expense)
         layoutManager = LinearLayoutManager(this@ExpenseActivity)
+        supportActionBar?.title = "Add Expense"
         val friendsList = GetFriends(applicationContext).execute().get()
         if(friendsList.size==0) {
             binding.txtDef.visibility = View.VISIBLE
             binding.recyclerList.visibility = View.GONE
-            binding.btnSplit.visibility = View.GONE
             binding.txtCheck.visibility = View.GONE
-            binding.layoutBill.visibility = View.GONE
         } else {
             binding.txtDef.visibility = View.GONE
             binding.recyclerList.visibility = View.VISIBLE
-            binding.btnSplit.visibility = View.VISIBLE
             binding.txtCheck.visibility = View.VISIBLE
             binding.layoutBill.visibility = View.VISIBLE
         }
         recyclerAdapter = FriendListAdapter(this@ExpenseActivity,friendsList)
         binding.recyclerList.layoutManager = layoutManager
         binding.recyclerList.adapter = recyclerAdapter
-
         binding.btnSplit.setOnClickListener {
-            val intent = Intent(this@ExpenseActivity,FinalActivity::class.java)
-            intent.putExtra("ListOfFriends",recyclerAdapter.checkList)
-            startActivity(intent)
+            if(checker(binding.etCost.text.toString().trim())) {
+                val intent = Intent(this@ExpenseActivity, FinalActivity::class.java)
+                intent.putExtra("ListOfFriends", recyclerAdapter.checkList)
+                intent.putExtra("TotalCost",binding.etCost.text)
+                startActivity(intent)
+            }
         }
     }
+
+    private fun checker(finalString: String): Boolean {
+        if(finalString.isEmpty()) {
+            binding.etCost.error = "Required"
+            binding.etCost.requestFocus()
+            return false
+        } else if(finalString[0]=='.' || finalString[finalString.length-1]=='.') {
+            binding.etCost.error = "Enter valid cost"
+            binding.etCost.requestFocus()
+            return false
+        } else {
+            val ind = finalString.indexOf('.')
+            val dec = finalString.substring(ind)
+            if(dec.length>2 || dec.contains('.')) {
+                binding.etCost.error = "Enter valid cost"
+                binding.etCost.requestFocus()
+                return false
+            } else return true
+        }
+    }
+
 }
--- app/src/main/java/com/himanshu/billsplit/FinalActivity.kt
+++ app/src/main/java/com/himanshu/billsplit/FinalActivity.kt
@@ -10,12 +10,11 @@ import com.himanshu.billsplit.databinding.ActivityFinalBinding
 class FinalActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFinalBinding
-    private var cost = 0f
+    var cost = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this@FinalActivity,R.layout.activity_final)
-        cost = 102030.12f
-        binding.cost = cost
+        binding.txtCost.text = "$cost"
         val list = this.intent.extras?.getParcelableArrayList<FriendEntity>("ListOfFriends")
         Toast.makeText(this,"$list",Toast.LENGTH_LONG).show()
     }
